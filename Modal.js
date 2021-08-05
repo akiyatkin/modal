d834f8db3270e7540e7fe586c5d3ba92a3f195f1
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2021-08-05 16:58:44 +0400

    fix

--- Modal.js
+++ Modal.js
@@ -2,36 +2,42 @@ const cls = (cls, el = div) => el.getElementsByClassName(cls)
 const wm = new WeakMap() 
 const Modal = {
 	_wm: wm,
-	getState: div => {
-		let state = wm.get(div)
+	getState: modal => {
+		let state = wm.get(modal)
 		if (!state) {
-			state = {fnshows:[], fnhides:[], is: false}
-			wm.set(div, state)
+			state = {fnshows:[], fnhides:[], is: false, lastfocus: false}
+			wm.set(modal, state)
 		}
 		return state
 	},
 	is: div => {
-		const state = Modal.getState(div)
+		const modal = cls('popupmodal', div)[0]
+		const state = Modal.getState(modal)
 		return state.is
 	},
 	hide: div => {
 		const modal = cls('popupmodal', div)[0]
 		modal.classList.remove('show')
-		const state = Modal.getState(div)
+		const state = Modal.getState(modal)
 		state.is = false
+		if (state.lastfocus) {
+			state.lastfocus.focus({ preventScroll: true })
+		}
 		for (const fnhide of state.fnhides) fnhide()
 	},
 	show: div => {
 		const modal = cls('popupmodal', div)[0]
 		modal.classList.add('show')
 		const close = cls('close', modal)[0]
-		close.focus()
-		const state = Modal.getState(div)
-		state.is = true
+		const state = Modal.getState(modal)
+		state.is = true		
+		state.lastfocus = document.activeElement
+		modal.focus({ preventScroll: true })
 		for (const fnshow of state.fnshows) fnshow()
 	},
 	showhide: (div, fnshow, fnhide) => {
-		const state = Modal.getState(div)
+		const modal = cls('popupmodal', div)[0]
+		const state = Modal.getState(modal)
 		if (fnshow) state.fnshows.push(fnshow)
 		if (fnhide) state.fnhides.push(fnhide)
 	}
