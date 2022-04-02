2e528ed757ea23b543d18201e9bd51a2d1c22530
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2022-04-02 16:10:24 +0400

    fix

--- Modal.js
+++ Modal.js
@@ -1,4 +1,4 @@
-const cls = (cls, el = div) => el.getElementsByClassName(cls)
+const cls = (cls, el) => el.getElementsByClassName(cls)
 const wm = new WeakMap() 
 const Modal = {
 	_wm: wm,
@@ -11,12 +11,12 @@ const Modal = {
 		return state
 	},
 	is: div => {
-		const modal = cls('popupmodal', div)[0]
+		const modal = div.classList.contains('popupmodal') ? div : cls('popupmodal', div)[0]
 		const state = Modal.getState(modal)
 		return state.is
 	},
 	hide: div => {
-		const modal = cls('popupmodal', div)[0]
+		const modal = div.classList.contains('popupmodal') ? div : cls('popupmodal', div)[0]
 		modal.classList.remove('show')
 		const state = Modal.getState(modal)
 		state.is = false
@@ -26,7 +26,7 @@ const Modal = {
 		for (const fnhide of state.fnhides) fnhide()
 	},
 	show: div => {
-		const modal = cls('popupmodal', div)[0]
+		const modal = div.classList.contains('popupmodal') ? div : cls('popupmodal', div)[0]
 		modal.classList.add('show')
 		const close = cls('close', modal)[0]
 		const state = Modal.getState(modal)
@@ -36,11 +36,52 @@ const Modal = {
 		for (const fnshow of state.fnshows) fnshow()
 	},
 	showhide: (div, fnshow, fnhide) => {
-		const modal = cls('popupmodal', div)[0]
+		const modal = div.classList.contains('popupmodal') ? div : cls('popupmodal', div)[0]
 		const state = Modal.getState(modal)
 		if (fnshow) state.fnshows.push(fnshow)
 		if (fnhide) state.fnhides.push(fnhide)
+	},
+	init: () => {
+		for (const modal of cls('popupmodal', document.body)) {
+			const state = Modal.getState(modal)
+			if (state.init) continue
+			state.init = true
+			const body = cls('body', modal)[0]
+			const close = cls('close', modal)[0]
+			if (close) {
+				close.addEventListener('click', async () => {
+					if (!modal.classList.contains('show')) return
+					Modal.hide(modal)
+				})
+				close.addEventListener('keypress', e => { //32 space, 13 enter
+					if (e.keyCode != 13) return
+					if (!modal.classList.contains('show')) return
+					e.preventDefault()
+					Modal.hide(modal)
+				})
+			}
+			document.body.addEventListener('click', async e => {
+				if (!modal.classList.contains('show')) return
+				let el = e.target
+				const path = [el]
+				while (el && el.parentElement) path.push(el = el.parentElement)
+				if (path.find(el => el.tagName == 'A')) return Modal.hide(modal); //Клик по ссылке закрываем
+				if (path.find(el => el == body)) return; //Клик внутри меню, меню не сворачивает
+				//Modal.hide(div)
+			}, true);
+			modal.addEventListener('keydown', async e => {
+				if (e.keyCode !== 13) return
+				if (e.target != modal) return
+				if (!modal.classList.contains('show')) return
+			    Modal.hide(modal)
+			});
+			document.body.addEventListener('keydown', async e => {
+				if (e.keyCode !== 27) return
+			    if (!modal.classList.contains('show')) return
+			    Modal.hide(modal)
+			});
+		}
 	}
 }
-window.Modal = Modal;
+
 export { Modal }
